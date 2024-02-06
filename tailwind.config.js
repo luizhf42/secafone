4bd14b64e0288a79f75f1586338652883e100391
luizhf42 <luizhf42@gmail.com>
2024-02-06 18:08:25 -0300

    feat: change background

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ export default {
 			fontFamily: {
 				sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
 			},
+			background: {
+				start: "rgba(73, 83, 82, 1)",
+				stop: "rgba(24, 33, 32, 1)",
+			},
 		},
 	},
 	plugins: [
@@ -15,8 +19,13 @@ export default {
 			addComponents({
 				".button-pulse": {
 					"box-shadow": "0 0 0 0 rgba(131, 188, 232, 0.7)",
-          "animation": "pulse 1.5s infinite cubic-bezier(0.66, 0, 0, 1)",
-          "transition": "500ms ease-in-out"
+					animation: "pulse 1.5s infinite cubic-bezier(0.66, 0, 0, 1)",
+					transition: "500ms ease-in-out",
+				},
+				".main-background": {
+					background: "rgb(73, 83, 82)",
+					background:
+						"radial-gradient(circle, rgba(73, 83, 82, 1) 0%, rgba(24, 33, 32, 1) 100%)",
 				},
 			});
 		}),
