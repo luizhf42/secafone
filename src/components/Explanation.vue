afdf2dbfece0ee8e7a1bcc20565174d28a7e39e4
luizhf42 <luizhf42@gmail.com>
2024-02-14 17:49:36 -0300

    feat: add explanation section

---
+++ src/components/Explanation.vue
@@ -0,0 +1,29 @@
+<template>
+	<section>
+		<p>
+			O <strong>Seca Fone</strong> utiliza sons para expelir água de
+			dispositivos eletrônicos, como smartphones, tablets, notebooks, etc. O
+			contato da água com a parte interna desses dispositivos pode causar
+			diversos problemas como corrosão e oxidação, danificando-os, muitas vezes,
+			de forma permanente. A vibração sonora ajuda a mover e expelir a água
+			através da saída de som. Use com cuidado e evite volumes muito altos.
+		</p>
+		<img src="../../assets/images/illustration.jpg" alt="" />
+	</section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="postcss" scoped>
+section {
+	@apply my-10 flex flex-col md:flex-row items-center justify-normal md:justify-between gap-8 w-4/5 mx-auto;
+
+	p {
+		@apply md:text-lg lg:text-xl w-full text-justify md:w-2/3;
+	}
+
+	img {
+		@apply w-full max-w-md md:w-1/4;
+	}
+}
+</style>
