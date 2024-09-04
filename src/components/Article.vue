0a43de0b659635d95cb51f3cb1bfa9ca7d794f73
luizhf42 <luizhf42@gmail.com>
2024-09-04 17:35:12 -0300

    fix: improve article design and add image

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -2,8 +2,24 @@
 	<div class="wrapper">
 		<router-link to="/">← Voltar</router-link>
 		<main>
-			<h1>{{ articles[slug].title }}</h1>
-			<div v-html="articles[slug].content"></div>
+			<h1>{{ article.title }}</h1>
+			<div class="info">
+				<span
+					>Escrito por
+					<a
+						href="https://www.linkedin.com/in/luizhf42"
+						rel="noopener noreferrer"
+						target="_blank"
+						>Luiz Henrique</a
+					></span
+				>
+				<span>Publicado em {{ formatArticleDate(article.publishedAt) }}</span>
+				<span v-if="article.updatedAt"
+					>Atualizado em {{ formatArticleDate(article.updatedAt) }}</span
+				>
+			</div>
+			<img :src="`../../assets/images/${slug}.webp`" alt="" />
+			<div class="content" v-html="article.content"></div>
 		</main>
 	</div>
 </template>
@@ -11,23 +27,34 @@
 <script setup lang="ts">
 import { inject } from "vue";
 import { useRoute } from "vue-router";
+import formatArticleDate from "../utils/formatArticleDate";
+
 const slug = useRoute().params.articleSlug as string;
 const articles = inject("articles") as any;
+const article = articles[slug];
 </script>
 
 <style scoped lang="postcss">
 .wrapper {
-	@apply w-10/12 max-w-xl mx-auto pt-4;
+	@apply w-10/12 max-w-3xl mx-auto py-4;
 
 	a {
-		@apply font-bold text-anchor;
+		@apply font-bold;
 	}
 
 	h1 {
-		@apply text-2xl lg:text-3xl font-bold drop-shadow-lg my-4;
+		@apply text-3xl lg:text-3xl font-bold drop-shadow-lg my-4;
 	}
 
-	:deep(h3) {
+	.info {
+		@apply flex flex-col my-4;
+	}
+
+	img {
+		@apply object-cover max-h-64 w-full my-4;
+	}
+
+	:deep(h2) {
 		@apply text-lg lg:text-xl font-bold;
 	}
 
