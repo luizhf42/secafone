2bdd99fcef09f53c240854d7ff7a6e7626249a04
luizhf42 <luizhf42@gmail.com>
2024-09-06 17:34:15 -0300

    feat: make scroll to top when path changes

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,9 @@ const routes = [
 const router = createRouter({
 	history: createWebHistory(),
 	routes,
+	scrollBehavior() {
+		return { top: 0 };
+	},
 });
 
 createApp(App).use(router).provide("articles", articles).mount("#app");
